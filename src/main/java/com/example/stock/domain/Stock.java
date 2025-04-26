920c260d8fc3ba23d02ab6792b6f2ee47830db7f
ChaedongIm <chaedong.im.dev@gmail.com>
2025-04-26 11:05:07 +0900

    feat: 재고 감소 로직

---
+++ src/main/java/com/example/stock/domain/Stock.java
@@ -0,0 +1,34 @@
+package com.example.stock.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+public class Stock {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long productId;
+
+    @Getter
+    private Long quantity;
+
+    public Stock() {
+
+    }
+
+    public Stock(Long productId, Long quantity) {
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+    public void decrease(Long quantity) {
+        if (this.quantity - quantity < 0) {
+            throw new RuntimeException("재고는 0개 미만이 될 수 없습니다.");
+        }
+
+        this.quantity -= quantity;
+    }
+}
