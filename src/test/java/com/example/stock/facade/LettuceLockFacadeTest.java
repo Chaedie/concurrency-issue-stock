9ca710b7b4c24829c6157d1affe4e0e594078e7d
ChaedongIm <chaedong.im.dev@gmail.com>
2025-04-28 17:19:39 +0900

    test: redis - lettuce 의 setnx 를 통한 분산락 구현

---
+++ src/test/java/com/example/stock/facade/LettuceLockFacadeTest.java
@@ -0,0 +1,63 @@
+package com.example.stock.facade;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class LettuceLockFacadeTest {
+    @Autowired
+    private LettuceLockFacade stockService;
+
+    @Autowired
+    private StockRepository stockRepository;
+
+    @BeforeEach
+    public void before() {
+        stockRepository.saveAndFlush(new Stock(1L, 100L));
+    }
+
+    @AfterEach
+    public void after() {
+        stockRepository.deleteAll();
+    }
+
+
+    @Test
+    public void 동시에_100개의_재고감소_요청() throws InterruptedException {
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        // 100 - (1 * 100) = 0 <- 이라고 예상하지만 아님 (동시성 이슈 발생)
+        assertEquals(0L, stock.getQuantity());
+
+        System.out.println("stock = " + stock);
+    }
+}
